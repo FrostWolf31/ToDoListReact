2059133a61313c82b8462aea37925210c0e17193
Steven Champagne <71238820+FrostWolf31@users.noreply.github.com>
2024-08-19 17:12:38 -0500

    TaskItem Added
    Items now are added to the tasklist and have buttons for future functionality.

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+//styles
+import styles from './TaskItem.module.css';
+
+//icons
+import { CheckIcon } from '@heroicons/react/24/outline'
+import { PencilIcon } from '@heroicons/react/24/outline'
+import { TrashIcon } from '@heroicons/react/24/solid';
+
+const TaskItem = ({task}) => {
+    const [isChecked, setIsChecked]= useState(task.checked);
+
+    const handleCheckboxChange = (e) =>{
+        setIsChecked(!isChecked);
+    }
+
+  return (
+    <div>
+        <li className={styles.task}>
+           <div className={styles["task-group"]}>
+            <input
+             type='checkbox'
+             className={styles.checkbox}
+             checked={isChecked}
+             onChange={handleCheckboxChange}
+             name = {task.name}
+             id={task.id}
+            />
+            <label 
+            htmlFor={task.id}
+            className={styles.label}
+            >
+            {task.name}    
+            <p className={styles.checkmark}>
+                <CheckIcon strokeWidth={2} width={24} height={24}/>
+            </p>
+            </label>          
+            </div> 
+            <div className={styles['task-group']}>
+                <button className='btn' aria-label={'Update $(task.name} Task'} 
+                //</div>onClick={}// 
+                >
+                     <PencilIcon width={24} height={24}/>
+                </button>
+
+                <button className={'btn ${styles.delete}'} 
+                aria-label={'Delete $(task.name} Task'} 
+                //</div>onClick={}// 
+                >
+                     <TrashIcon width={24} height={24}/>
+                </button>
+            </div>
+
+            
+        </li>    
+    </div>
+  )
+}
+
+export default TaskItem
